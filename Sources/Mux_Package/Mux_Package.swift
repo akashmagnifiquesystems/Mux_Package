4a72f5b5086c6c486923e28827d81f3c1580a9aa
Akash Patel <aakashpatel.iosdev@gmail.com>
2021-08-20 19:19:26 +0530

    -- commit

--- Sources/Mux_Package/Mux_Package.swift
+++ Sources/Mux_Package/Mux_Package.swift
@@ -3,6 +3,7 @@ public struct Mux_Package {
 
     let authUserName: String = "3c942eb3-d47b-4e74-8680-83b9c24dac51"
     let authPassword: String = "mxvVn2sFiu9nuvmMyxDntqQsFT3HaZP1a5+DRQeKqs/9idK5Jb80HXhQtgTgOcMwKGFD5P+g2fS"
+    let baseURL: String = "https://api.mux.com/video/v1/live-streams"
 
     public init() {
         
@@ -16,7 +17,7 @@ public struct Mux_Package {
         let loginData = loginString.data(using: String.Encoding.utf8)!
         let base64LoginString = loginData.base64EncodedString()
 
-        var request = URLRequest(url: URL(string: "https://api.mux.com/video/v1/live-streams")!)
+        var request = URLRequest(url: URL(string: self.baseURL)!)
         request.httpMethod = "POST"
         request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
         request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
@@ -38,16 +39,14 @@ public struct Mux_Package {
     }
 
     //MARK:- Restrive Live Streaming
-    public func call_Retrive_Streaming(LIVE_STREAM_ID : String, completion: @escaping (NSDictionary) -> Void) {
-        let params = [:] as Dictionary<String, AnyObject>
+    public func call_Retrive_Streaming(Live_Stream_ID : String, completion: @escaping (NSDictionary) -> Void) {
         
         let loginString = String(format: "%@:%@", authUserName, authPassword)
         let loginData = loginString.data(using: String.Encoding.utf8)!
         let base64LoginString = loginData.base64EncodedString()
 
-        var request = URLRequest(url: URL(string: "https://api.mux.com/video/v1/live-streams/\(LIVE_STREAM_ID)")!)
+        var request = URLRequest(url: URL(string: "\(self.baseURL)/\(Live_Stream_ID)")!)
         request.httpMethod = "GET"
-//        request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
         request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
 
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -66,14 +65,14 @@ public struct Mux_Package {
     }
     
     //MARK:- Stop Live Streaming
-    public func call_Disable_Streaming(LIVE_STREAM_ID : String, completion: @escaping (NSDictionary) -> Void) {
+    public func call_Disable_Streaming(Live_Stream_ID : String, completion: @escaping (NSDictionary) -> Void) {
         let params = [:] as Dictionary<String, AnyObject>
         
         let loginString = String(format: "%@:%@", authUserName, authPassword)
         let loginData = loginString.data(using: String.Encoding.utf8)!
         let base64LoginString = loginData.base64EncodedString()
 
-        var request = URLRequest(url: URL(string: "https://api.mux.com/video/v1/live-streams/\(LIVE_STREAM_ID)/disable")!)
+        var request = URLRequest(url: URL(string: "\(self.baseURL)/\(Live_Stream_ID)/disable")!)
         request.httpMethod = "PUT"
         request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
         request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
