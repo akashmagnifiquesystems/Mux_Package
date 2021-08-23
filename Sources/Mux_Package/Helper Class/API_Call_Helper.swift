6f12f5e7e93a4a5d9f98155c3a91fd9258cf5a43
Akash Patel <aakashpatel.iosdev@gmail.com>
2021-08-23 18:19:08 +0530

    -- commit

--- Sources/Mux_Package/Helper Class/API_Call_Helper.swift
+++ Sources/Mux_Package/Helper Class/API_Call_Helper.swift
@@ -1,20 +1,50 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aakash Patel on 23/08/21.
+//
+
+import Foundation
 import UIKit
 
-public struct Mux_Package {
+public struct API_Call_Helper {
 
     let authUserName: String = "3c942eb3-d47b-4e74-8680-83b9c24dac51"
     let authPassword: String = "mxvVn2sFiu9nuvmMyxDntqQsFT3HaZP1a5+DRQeKqs/9idK5Jb80HXhQtgTgOcMwKGFD5P+g2fS"
     let baseURL: String = "https://api.mux.com/video/v1/live-streams"
-    let apiCallHelper = API_Call_Helper()
+
     public init() {
         
     }
     
     //MARK:- Create Live Streaming
     public func call_Create_Live_Streaming(completion: @escaping (NSDictionary) -> Void) {
-        apiCallHelper.call_Create_Live_Streaming { response in
-            completion(response)
-        }
+        let params = ["playback_policy":"public", "new_asset_settings":["playback_policy":"public",]] as Dictionary<String, AnyObject>
+        
+        let loginString = String(format: "%@:%@", authUserName, authPassword)
+        let loginData = loginString.data(using: String.Encoding.utf8)!
+        let base64LoginString = loginData.base64EncodedString()
+
+        var request = URLRequest(url: URL(string: self.baseURL)!)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+                print(json)
+                completion(json as NSDictionary)
+            } catch {
+                print("error")
+            }
+        })
+
+        task.resume()
     }
 
     //MARK:- List Live Streaming
